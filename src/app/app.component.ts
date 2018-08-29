7ffee8849c44a90e480760727356d0444536d96f
BuntyBru <buntybru@gmail.com>
2018-08-29 14:44:32 +0530

    make it better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'project';
+  loadedFeature = 'recipe';
+
+  ngOnInit(){
+  	firebase.initializeApp({
+  	apiKey: "AIzaSyDlMqI3pgBdEXk4QsVHO_yKQDI3Sb2Y_18",
+    authDomain: "ng-recipe-book-e101c.firebaseapp.com"
+  	});
+  }
+
+
+  onNavigate(featured: string)
+  {
+  this.loadedFeature = featured;
+
+  }
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,75 @@
+import { Recipe } from './recipe.model';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import {Subject} from 'rxjs/Subject';
+@Injectable()
+
+
+export class RecipeService{
+
+recipesChanged=new Subject<Recipe[]>();
+
+	private recipes: Recipe[]=[
+
+new Recipe(
+'Tasty Schnitzel',
+'A super tasty Schnitzel- just awesome',
+'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Wiener-Schnitzel02.jpg/1024px-Wiener-Schnitzel02.jpg',
+[
+new Ingredient('Meat',1 ),
+new Ingredient('French fries', 20)]),
+new Recipe('Big Fat Burger',
+'It cannot get any bigger than this',
+'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246__340.jpg',
+[ new Ingredient ('Buns',2),
+new Ingredient('Meat',1),
+new Ingredient('mayo',1)
+]),
+
+
+];
+
+constructor(private slService: ShoppingListService){}
+
+setRecipes(recipes:Recipe[])
+{
+	this.recipes=recipes;
+	this.recipesChanged.next(this.recipes.slice());
+}
+
+
+getRecipes(){
+	return this.recipes.slice();
+}
+
+
+getRecipe(id:number)
+{
+	return this.recipes[id];
+}
+
+addIngredientsToShoppingList(ingredients: Ingredient[]){
+this.slService.addIngredients(ingredients);
+	
+}
+
+addRecipe(recipe:Recipe)
+{
+	this.recipes.push(recipe);
+	this.recipesChanged.next(this.recipes.slice());
+}
+
+updateRecipe(index:number,newRecipe:Recipe)
+{
+	this.recipes[index]=newRecipe;
+	this.recipesChanged.next(this.recipes.slice());
+
+}
+
+deleteRecipe(index:number)
+{
+	this.recipes.splice(index,1);
+	this.recipesChanged.next(this.recipes.slice());
+}
+}
